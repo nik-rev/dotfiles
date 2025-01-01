b2c80409df35d78c5744e8e4aff516c087ef6214
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-01-01 02:29:25 +0000

    feat: update catppuccin theme for nushell

--- zsh.nix
+++ zsh.nix
@@ -156,33 +156,68 @@
           operator: $theme.sky
           string: $theme.green
           virtual_text: $theme.surface2
+          variable: { fg: $theme.flamingo attr: i }
+          filepath: $theme.yellow
         }
 
         $env.config.color_config = {
           separator: { fg: $theme.surface2 attr: b }
-          leading_trailing_space_bg: { attr: n }
+          leading_trailing_space_bg: { fg: $theme.lavender attr: u }
           header: { fg: $theme.text attr: b }
+          row_index: $scheme.virtual_text
+          record: $theme.text
+          list: $theme.text
+          hints: $scheme.virtual_text
+          search_result: { fg: $theme.base bg: $theme.blue }
+          shape_closure: $theme.teal
+          closure: $theme.teal
+          shape_flag: { fg: $theme.maroon attr: i }
+          shape_matching_brackets: { attr: u }
+          shape_garbage: $theme.red
+          shape_keyword: $theme.mauve
+          shape_match_pattern: $theme.green
+          shape_signature: $theme.teal
+          shape_table: $scheme.punctuation
+          cell-path: $scheme.punctuation
+          shape_list: $scheme.punctuation
+          shape_record: $scheme.punctuation
+          shape_vardecl: $scheme.variable
+          shape_variable: $scheme.variable
           empty: { attr: n }
-          bool: $scheme.constant
-          int: $scheme.constant
-          filesize: {|fsize|
-            if $fsize < 1kb {
+          filesize: {||
+            if $in < 1kb {
               $theme.teal
-            } else if $fsize < 10kb {
+            } else if $in < 10kb {
               $theme.green
-            } else if $fsize < 100kb {
+            } else if $in < 100kb {
               $theme.yellow
-            } else if $fsize < 10mb {
+            } else if $in < 10mb {
               $theme.peach
-            } else if $fsize < 100mb {
+            } else if $in < 100mb {
               $theme.maroon
-            } else if $fsize < 1gb {
+            } else if $in < 1gb {
               $theme.red
             } else {
               $theme.mauve
             }
           }
-          duration: $scheme.constant
+          duration: {||
+            if $in < 1day {
+              $theme.teal
+            } else if $in < 1wk {
+              $theme.green
+            } else if $in < 4wk {
+              $theme.yellow
+            } else if $in < 12wk {
+              $theme.peach
+            } else if $in < 24wk {
+              $theme.maroon
+            } else if $in < 52wk {
+              $theme.red
+            } else {
+              $theme.mauve
+            }
+          }
           date: {|| (date now) - $in |
             if $in < 1day {
               $theme.teal
@@ -200,57 +235,55 @@
               $theme.mauve
             }
           }
+          shape_external: $scheme.unrecognized_command
+          shape_internalcall: $scheme.recognized_command
+          shape_external_resolved: $scheme.recognized_command
+          shape_block: $scheme.recognized_command
+          block: $scheme.recognized_command
+          shape_custom: $theme.pink
+          custom: $theme.pink
           background: $theme.base
           foreground: $theme.text
+          cursor: { bg: $theme.text fg: $theme.base }
+          shape_range: $scheme.operator
           range: $scheme.operator
+          shape_pipe: $scheme.operator
+          shape_operator: $scheme.operator
+          shape_redirection: $scheme.operator
+          glob: $scheme.filepath
+          shape_directory: { fg: $scheme.filepath attr: b }
+          shape_filepath: $scheme.filepath
+          shape_glob_interpolation: $scheme.filepath
+          shape_globpattern: $scheme.filepath
+          shape_int: $scheme.constant
+          int: $scheme.constant
+          bool: $scheme.constant
           float: $scheme.constant
-          string: $scheme.string
           nothing: $scheme.constant
           binary: $scheme.constant
-          cell-path: $scheme.punctuation
-          row_index: $theme.subtext1
-          record: $theme.text
-          list: $theme.text
-          hints: $scheme.virtual_text
-          search_result: { attr: r }
-          shape_closure: $theme.teal
-          shape_directory: $theme.blue
-          shape_externalarg: $theme.text
-          shape_filepath: $theme.blue
-          shape_flag: { fg: $theme.maroon attr: i }
-          shape_globpattern: $theme.text
-          shape_int: $scheme.constant
-          shape_internalcall: $scheme.recognized_command
-          shape_list: $scheme.punctuation
-          shape_matching_brackets: { attr: u }
           shape_nothing: $scheme.constant
-          shape_pipe: $scheme.operator
-          shape_record: $scheme.punctuation
-          shape_string: $scheme.string
-          shape_string_interpolation: $theme.flamingo
-          shape_raw_string: $scheme.string
-          shape_garbage: $theme.red
-          shape_keyword: $theme.mauve
-          shape_block: $theme.blue
-          shape_match_pattern: $theme.green
-          shape_operator: $scheme.operator
-          shape_table: $scheme.punctuation
-          shape_variable: { fg: $theme.peach attr: i }
           shape_bool: $scheme.constant
-          shape_signature: $theme.teal
-          shape_vardecl: { fg: $theme.peach attr: i }
-          shape_external: $scheme.unrecognized_command
-          shape_range: $scheme.operator
-          shape_redirection: { fg: $theme.text attr: b }
           shape_float: $scheme.constant
           shape_binary: $scheme.constant
           shape_datetime: $scheme.constant
-          shape_external_resolved: $scheme.recognized_command
-          shape_custom: { fg: "#ff0000" bg: "#ff0000" }
-          glob: { fg: "#00ff00" bg: "#00ff00" }
-          shape_literal: { fg: "#ffff00" bg: "#ffff00" }
-          shape_glob_interpolation: { fg: "#ff00ff" bg: "#ff00ff" }
-          block: { fg: "#00ffff" bg: "#00ffff" }
+          shape_literal: $scheme.constant
+          string: $scheme.string
+          shape_string: $scheme.string
+          shape_string_interpolation: $theme.flamingo
+          shape_raw_string: $scheme.string
+          shape_externalarg: $scheme.string
+        }
+        $env.config.highlight_resolved_externals = true
+        $env.config.explore = {
+            status_bar_background: { fg: $theme.text, bg: $theme.mantle },
+            command_bar_text: { fg: $theme.text },
+            highlight: { fg: $theme.base, bg: $theme.yellow },
+            status: {
+                error: $theme.red,
+                warn: $theme.yellow,
+                info: $theme.blue,
+            },
+            selected_cell: { bg: $theme.blue fg: $theme.base },
         }
         $env.config.highlight_resolved_externals = true
       '';
