539812812d8e78d2ae15bbc1468d95a8a706dd3d
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-01-04 03:57:54 +0000

    feat: format wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -8,47 +8,23 @@ config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 config.automatically_reload_config = false
 config.swallow_mouse_click_on_window_focus = true
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 config.adjust_window_size_when_changing_font_size = false
 
 config.keys = {
-	{
-		key = "F11",
-		action = wezterm.action.ToggleFullScreen,
-	},
-	{
-		key = "+",
-		mods = "CTRL",
-		action = wezterm.action.IncreaseFontSize,
-	},
-	{
-		key = "-",
-		mods = "CTRL",
-		action = wezterm.action.DecreaseFontSize,
-	},
-	{
-		key = "=",
-		mods = "CTRL",
-		action = wezterm.action.ResetFontSize,
-	},
+	{ key = "F11", action = wezterm.action.ToggleFullScreen },
+	{ key = "+", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
+	{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
+	-- TODO: restore these once i figure out https://github.com/wez/wezterm/discussions/6550
+	-- { key = "u", mods = "CTRL", action = wezterm.action.ScrollByPage(-1) },
+	-- { key = "d", mods = "CTRL", action = wezterm.action.ScrollByPage(1) },
+	{ key = "=", mods = "CTRL", action = wezterm.action.ResetFontSize },
+	-- { key = "Enter", mods = "SHIFT", action = wezterm.action.disableDefaultAssignment },
 	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
 	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
-	{
-		key = "\\",
-		mods = "CTRL",
-		action = wezterm.action.ActivateCopyMode,
-	},
-	{
-		key = "Backspace",
-		mods = "CTRL",
-		action = wezterm.action.SendKey({ mods = "CTRL", key = "w" }),
-	},
+	{ key = "\\", mods = "CTRL", action = wezterm.action.ActivateCopyMode },
+	{ key = "Backspace", mods = "CTRL", action = wezterm.action.SendKey({ mods = "CTRL", key = "w" }) },
 }
 
 config.char_select_font_size = 13
