a4af7c3656e555146f05395915ba537485d292df
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-06-07 18:34:06 +0100

    fix: disable duplicate blueman

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,10 @@ in
     enable = true;
     config = {
       sway = {
-        default = ["wlr" "gtk"];
+        default = [
+          "wlr"
+          "gtk"
+        ];
       };
     };
     xdgOpenUsePortal = true;
@@ -110,7 +113,6 @@ in
   services = {
     blueman.enable = true;
     libinput.enable = true;
-    blueman.enable = true;
     # adds all executables to /usr/bin to be able to run
     # various scripts on NixOS
     envfs.enable = true;
