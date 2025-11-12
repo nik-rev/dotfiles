789b7abf1e321d2dac60cf71ea33a9fa7bdfd874
Nik Revenco <pm@nikrev.com>
2025-11-12 21:49:43 +0000

    Update GlazeWM and Sway config

--- sway.nix
+++ sway.nix
@@ -16,33 +16,38 @@
       };
       bars = [ ];
       keybindings = {
+        "Mod1+m" = "exec ferrishot";
+        "Mod1+i" = "exec kitty";
+        "Mod1+r" = "exec firefox";
+
+        "Mod1+t" = "kill";
+
+        "Mod1+e" = "focus left";
+        "Mod1+o" = "focus right";
+
+        "Mod1+2" = "move left";
+        "Mod1+4" = "move down";
+        "Mod1+1" = "move up";
+        "Mod1+8" = "move right";
+
+        "Mod1+w" = "workspace number 1";
+        "Mod1+f" = "workspace number 2";
+        "Mod1+p" = "workspace number 3";
+        "Mod1+b" = "workspace number 4";
+
+        "Mod1+5" = "move container to workspace number 1";
+        "Mod1+0" = "move container to workspace number 2";
+        "Mod1+3" = "move container to workspace number 3";
+        "Mod1+7" = "move container to workspace number 4";
+
+        "Mod1+u" = "resize grow width 10 px or 10 ppt";
+        "Mod1+y" = "resize shrink width 10 px or 10 ppt";
+
         "XF86AudioRaiseVolume" = "exec --no-startup-id pamixer --increase 5";
         "XF86AudioLowerVolume" = "exec --no-startup-id pamixer --decrease 5";
         "XF86AudioMute" = "exec --no-startup-id pamixer --toggle-mute";
         "XF86MonBrightnessUp" = "exec --no-startup-id lumina set +5%";
         "XF86MonBrightnessDown" = "exec --no-startup-id lumina set -5%";
-        "Mod1+m" = "exec ferrishot";
-        "Mod1+i" = "exec kitty";
-        "Mod1+r" = "exec firefox";
-        "Mod1+t" = "kill";
-        "Mod1+e" = "focus left";
-        "Mod1+o" = "focus right";
-        "Mod1+z" = "sticky toggle";
-        "Mod1+2" = "move left";
-        "Mod1+4" = "move down";
-        "Mod1+1" = "move up";
-        "Mod1+8" = "move right";
-        "Mod1+g" = "floating toggle";
-        "Mod1+w" = "workspace number 1";
-        "Mod1+f" = "workspace number 2";
-        "Mod1+p" = "workspace number 3";
-        "Mod1+b" = "workspace number 4";
-        "Mod1+5" = "move container to workspace number 1";
-        "Mod1+0" = "move container to workspace number 2";
-        "Mod1+3" = "move container to workspace number 3";
-        "Mod1+7" = "move container to workspace number 4";
-        "Mod1+u" = "resize grow width 10 px or 10 ppt";
-        "Mod1+y" = "resize shrink width 10 px or 10 ppt";
       };
       colors = {
         focused = {
